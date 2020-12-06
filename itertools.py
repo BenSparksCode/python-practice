9827637b69da064db59bcd6cdc997c68f6d4f74b
Ben Sparks <bensparks0@gmail.com>
2020-12-06 14:19:06 +0200

    Finished itertools

--- itertools.py
+++ itertools.py
@@ -4,11 +4,11 @@ from itertools import product
 from itertools import permutations
 from itertools import combinations
 from itertools import accumulate
+import operator #for changing accumulate operation
 from itertools import groupby
-# from itertools import
 
 # product: creates list of tuples forming combos of elements from lists - useful for matrices
-a = [2,3,4]
+a = [2,3,4,3]
 b = [5,6,7]
 prod = product(a, b)
 print(list(prod))
@@ -19,8 +19,22 @@ print(list(perm))
 perm = permutations(a, 2) #only length 2 perms
 print(list(perm))
 
-# combinations: 
+# combinations: creates list of tuples of all cominations - length needed
 comb = combinations(a, 2) #length arg is mandatory for combinations - no repetition
 print(list(comb))
 
-# accumulate: 
+# accumulate: creates list where items are sum of prev items - cumulative data
+acc = accumulate(a)
+print(list(acc))
+acc = accumulate(a, func=operator.mul) # sets operation to multiply, not adding
+print(list(acc))
+acc = accumulate(a, func=max) # sets operation to finding max in list so far
+print(list(acc))
+
+# groupby: groups list items into separate lists based on a decision function
+a = [1,2,3,4]
+
+group_obj = groupby(a, key=lambda x: x<3)
+
+for key, val in group_obj:
+    print(key, list(val))
